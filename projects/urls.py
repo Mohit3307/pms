81adcd462ba67bb8d7128bef98de8572a6be5e37
Axant Gondaliya <axantgondaliya0@gmail.com>
2026-02-27 23:13:14 +0530

    Implement Project CRUD functionality

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.project_list, name='project_list'),
+    path('create/', views.create_project, name='create_project'),
+    path('update/<int:pk>/', views.update_project, name='update_project'),
+    path('delete/<int:pk>/', views.delete_project, name='delete_project'),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/project_list.html', {'projects': projects})
+
+
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('project_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects/project_form.html', {'form': form})
+
+
+def update_project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('project_list')
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'projects/project_form.html', {'form': form})
+
+
+def delete_project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect('project_list')
+    return render(request, 'projects/project_confirm_delete.html', {'project': project})
