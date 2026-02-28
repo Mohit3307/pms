d7a2f919e54d98c7b090910113f18f722e6d3d27
Mohit3307 <jadavmohit5050@gmail.com>
2026-02-28 12:16:11 +0530

    fix: add project detail route and navigation

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project
 from .forms import ProjectForm
+from team.models import Team
 
 
 def project_list(request):
@@ -12,7 +13,17 @@ def create_project(request):
     if request.method == "POST":
         form = ProjectForm(request.POST)
         if form.is_valid():
-            form.save()
+
+            project = form.save(commit=False)
+            project.created_by = request.user
+            project.save()
+
+            Team.objects.create(
+                project=project,
+                user=request.user,
+                role="ADMIN"
+            )
+
             return redirect('project_list')
     else:
         form = ProjectForm()
@@ -37,3 +48,10 @@ def delete_project(request, pk):
         project.delete()
         return redirect('project_list')
     return render(request, 'projects/project_confirm_delete.html', {'project': project})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, "projects/project_detail.html", {
+        "project": project
+    })
