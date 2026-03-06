535a6f4ef3ed172dffb3ccfc64523816f55381f0
Koyani Dev Jagdishbhai <koyanidev81@gmail.com>
2026-03-07 02:48:27 +0530

    created dashboard view logic

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from projects.models import Project
+from tasks.models import Task
+from team.models import Team
 
-# Create your views here.
+
+def dashboard(request):
+
+    active_projects = Project.objects.count()
+
+    open_tasks = Task.objects.filter(status="Pending").count()
+
+    completed_tasks = Task.objects.filter(status="Completed").count()
+
+    team_members = Team.objects.count()
+
+    context = {
+        "active_projects": active_projects,
+        "open_tasks": open_tasks,
+        "completed_tasks": completed_tasks,
+        "team_members": team_members
+    }
+
+    return render(request, "dashboard.html", context)
