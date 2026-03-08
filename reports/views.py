2903aa1256549b393c61a81c8a1936ad0dc701fd
Axant Gondaliya <axantgondaliya0@gmail.com>
2026-03-08 14:29:08 +0530

    feat: add progress bar in project report

--- reports/views.py
+++ reports/views.py
@@ -14,9 +14,25 @@ def reports_dashboard(request):
 
 def project_report(request):
     projects = Project.objects.all()
+    project_data = []
+    for project in projects:
+        tasks = Task.objects.filter(project=project)
+        total_tasks = tasks.count()
+        completed_tasks = tasks.filter(status="Completed").count()
+        if total_tasks > 0:
+            progress = round((completed_tasks / total_tasks) * 100)
+        else:
+            progress = 0
+        if total_tasks > 0 and completed_tasks == total_tasks:
+            project.status = "completed"
+            project.save()
+        project_data.append({
+            "project": project,
+            "progress": progress
+        })
     project_ids = Team.objects.filter(user=request.user).values_list("project_id", flat=True)
     sidebar_projects = Project.objects.filter(id__in=project_ids)
-    return render(request, "reports/project_report.html", {"projects": projects,"sidebar_projects": sidebar_projects})
+    return render(request, "reports/project_report.html", {"project_data": project_data,"sidebar_projects": sidebar_projects})
 
 
 def task_report(request):
