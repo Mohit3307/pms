d86dfbbaea73e4ae7bafc4393270ad4fa2ed86cb
Axant Gondaliya <axantgondaliya0@gmail.com>
2026-03-08 13:15:34 +0530

    fix: update dashboard to correctly show projects, tasks, and team members

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,23 +6,32 @@ from team.models import Team
 
 def dashboard(request):
 
+    project_ids = Team.objects.filter(user=request.user)\
+                              .values_list("project_id", flat=True)
+
+    projects = Project.objects.filter(id__in=project_ids)
+
+    my_tasks = Task.objects.filter(assignee=request.user, status="Pending")
+
     active_projects = Project.objects.count()
 
     open_tasks = Task.objects.filter(status="pending").count()
 
-    completed_tasks = Task.objects.filter(status="completed").count()
+    completed_tasks = Task.objects.filter(id__in=project_ids,status="completed").count()
 
-    team_members = Team.objects.count()
+    team_members = Team.objects.filter(project_id__in=project_ids).count()
 
-    project_ids = Team.objects.filter(user=request.user).values_list("project_id", flat=True)
     sidebar_projects = Project.objects.filter(id__in=project_ids)
 
+    projects = sidebar_projects
     context = {
         "active_projects": active_projects,
         "open_tasks": open_tasks,
         "completed_tasks": completed_tasks,
         "team_members": team_members,
         "sidebar_projects": sidebar_projects,
+        "projects": projects,
+        "my_tasks": my_tasks,
     }
 
     return render(request, "dashboard.html", context)
